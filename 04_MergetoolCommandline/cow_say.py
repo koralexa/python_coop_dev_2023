a5bac97e09b060df8f08dafad1f32fff56528cf1
koralexa <koralexa@ispras.ru>
2023-03-05 15:23:07 +0300

    Add list_cows and make_bubble to cow command line

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -37,6 +37,43 @@ class CmdCowSay(cmd.Cmd):
   intro = "Welcome to cow command line"
   prompt = "mu> "
 
+  def do_list_cows(self, arg):
+    """
+		list_cows [dir]
+		Lists all cow file names in the given directory or default cow list
+		"""
+    cowpath = None
+    if arg: 
+      cowpath = shlex.split(arg)[0]
+    print(*cowsay.list_cows(cowpath))
+
+  def do_make_bubble(self, arg):
+    """
+		make_buble [wrap_text [width [brackets ]]]
+		The text that appears above the cows
+		"""
+    message, *options = shlex.split(arg)
+    wrap_text = True
+    width = 40
+    brackets = cowsay.THOUGHT_OPTIONS['cowsay']
+    if options:
+      if options[0]:
+        wrap_text = bool(options[0] == 'True')
+      if len(options) > 1 and options[1]:
+        width = int(options[1])
+      if len(options) > 2 and options[2]:
+        brackets = options[2]
+    print(cowsay.make_bubble(message, brackets=brackets, width=width, wrap_text=wrap_text))
+
+  def complete_make_bubble(self, text, line, begidx, endidx):
+    args = shlex.split(line)
+    args_num = len(args)
+    text_in_args = args[-1] == text
+    args_2 = args_num == 2
+    args_3 = args_num == 3
+    if (args_2 and not text_in_args or args_3 and text_in_args):
+      return [res for res in ['True', 'False'] if res.lower().startswith(text.lower())]
+
   def do_cowsay(self, arg):
     """
 		cowsay message [cow [eyes [tongue]]]
